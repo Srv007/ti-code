a3e52c1229084da9093ecb565fae21112c1499bb
Srv007 <saurabh@gbpuat-tech.ac.in>
2014-10-31 23:31:02 +0530

    init all variable

--- ti-adc.ino
+++ ti-adc.ino
@@ -1,15 +1,61 @@
 #include <TwoMsTimer.h>
 #include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+#define rfid_RX P1_5
+#define rfid_TX P6_0
+
+//Side button Configuration//
+#define button1 P4_3
+#define button2 P4_0
+#define button3 P8_2
+#define lock_button P4_0
+
+//vibration pin Configuration
+#define vibraton_pin P7_0
+
+//uart configuration for rf-id
+SoftwareSerial rfid(rfid_RX,rfid_TX);
+
+//Keypad pin interface pins:
+const int column[] = {
+  P4_1,P4_2,P2_7,P3_2};
+const int row[] = {
+  P6_6, P1_6, P1_2, P1_3};
+
 void setup(){
-  lcd.begin( 20, 4);
+  backlight_status=true;
+  pinMode( button1, INPUT_PULLUP); 
+  pinMode( button2, INPUT_PULLUP); 
+  pinMode( lock_button, INPUT_PULLUP); 
+  pinMode( button3, INPUT_PULLDOWN); 
+  pinMode( vibraton_pin, OUTPUT);
+  digitalWrite( vibraton_pin, LOW);
+  TwoMsTimer::set(200, automatic_bacllight);
+  TwoMsTimer::start();
+  Serial1.begin(9600);   // initialize UART communication @ 9600 bps serial port 1 is used pin P3.3 and P3.4 are used 
+  rfid.begin(9600); 
+  delay(1000);
   lcd.clear();
-  for(i=0;i<40;i++){
-    for(j=0;j<4;j++){
-      for(k=0;k<20;k++){
-        pnrdata[i][j][k]='\0';
-      }
-    }
+  lcd.print("       ATCAD");
+  // initialize UART communication with RF-ID @ 9600 bps software-serial is used
+  for (x=0; x<4; x++)  {
+    pinMode(column[x], OUTPUT);
   }
+  for (y=0; y<4; y++)  {
+    pinMode(row[y], INPUT_PULLUP);
+  } 
+ // connect();
+  //
+//  main_lock();
+  lcd.clear();
+  get_tte_number();
+  tte_code[12]='\0';
+  lcd.setCursor(0,3);
+  lcd.print("Press to continue...");
+  key();
+  lcd.createChar(2, black);
+  lcd.createChar(3, arrow);
+  backlight_status==true;
   
   
 }
