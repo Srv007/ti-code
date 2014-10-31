753877bb0ccb9c8f3f75e6784b7adf98afc6b96f
Srv007 <saurabh@gbpuat-tech.ac.in>
2014-10-31 23:30:07 +0530

    lcd init

--- ti-adc.ino
+++ ti-adc.ino
@@ -3,10 +3,18 @@
 void setup(){
   lcd.begin( 20, 4);
   lcd.clear();
+  for(i=0;i<40;i++){
+    for(j=0;j<4;j++){
+      for(k=0;k<20;k++){
+        pnrdata[i][j][k]='\0';
+      }
+    }
+  }
+  
   
 }
 void loop(){
-  while(1);
+ 
 }
 
 
